d60333f51cb2054494562754248851ecfdda3d64
Triple <sebastien.andrys@gmail.com>
2019-04-11 23:42:36 +0200

    change menu

--- menu.js
+++ menu.js
@@ -6,7 +6,7 @@ class Menu extends Phaser.Scene {
     create() {
         this.add.image(150, 150)
         this.add.text(20, 20, "Choose");
-        this.scene.start("levelLoader");
+        //this.scene.start("levelLoader");
     }
 
 }
